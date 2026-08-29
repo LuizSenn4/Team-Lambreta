145601fc9c38c664f50461d80228566d9205eab6
Senn4_Axor <132702416+LuizSenn4@users.noreply.github.com>
2026-08-29 13:45:20 -0400

    feat: ativa trofeu mix na sala de live

--- team-live-gifts-v1.js
+++ team-live-gifts-v1.js
@@ -6,5 +6,7 @@
     if (onload) s.onload = onload;
     document.body.appendChild(s);
   };
-  add('team-live-gifts-core-v2.js?v=2.0', () => add('team-live-commands-v1.js?v=1.0'));
+  add('team-live-gifts-core-v2.js?v=2.0', () => {
+    add('team-live-commands-v1.js?v=1.0', () => add('team-live-trophy-v1.js?v=1.0'));
+  });
 })();
